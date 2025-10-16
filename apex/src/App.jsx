5b7cc112f412d6418d45fc875c46df58234dddd9
hhye0 <jhy030123@naver.com>
2025-10-17 02:01:25 +0900

    layout 설정

--- apex/src/App.jsx
+++ apex/src/App.jsx
@@ -42,21 +42,21 @@ function App() {
 
 
   return (
-        <BrowserRouter>
-            <Routes>
-                {/* 1. 로그인 페이지 (보호되지 않음) */}
-                <Route 
-                    path="/login" 
-                    element={<Login setIsLoggedIn={setIsLoggedIn} />}
-                />
+    <BrowserRouter>
+        <Routes>
+            {/* 1. 로그인 페이지 (보호되지 않음) */}
+            <Route 
+                path="/login" 
+                element={<Login setIsLoggedIn={setIsLoggedIn} />}
+            />
 
-                {/* 2. 보호된 영역: 로그인 상태에 따라 ProtectedLayout 전체를 렌더링 */}
-                <Route
-                    path="/*"
-                    element={isLoggedIn ? ProtectedLayout : <Navigate to="/login" />}
-                />
-            </Routes>
-        </BrowserRouter>
+            {/* 2. 보호된 영역: 로그인 상태에 따라 ProtectedLayout 전체를 렌더링 */}
+            <Route
+                path="/*"
+                element={isLoggedIn ? ProtectedLayout : <Navigate to="/login" />}
+            />
+        </Routes>
+    </BrowserRouter>
     );
 }
 
